6ebf0aca67603bc0c8976a002418d33103fc4ee6
iruasev <jrua1984@gmail.com>
2017-10-12 23:01:30 +0200

    * Lexik JWT Configured

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as JMS;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\User\JWTUserInterface;
 
 /**
  * Users
@@ -12,7 +13,7 @@ use JMS\Serializer\Annotation as JMS;
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  * @JMS\ExclusionPolicy("none")
  */
-class User
+class User implements JWTUserInterface
 {
     /**
      * @var integer
@@ -206,6 +207,71 @@ class User
         return $this;
     }
 
+    /**
+     * Creates a new instance from a given JWT payload.
+     *
+     * @param string $username
+     * @param array $payload
+     *
+     * @return JWTUserInterface
+     */
+    public static function createFromPayload($username, array $payload)
+    {
+        // TODO: Implement createFromPayload() method.
+    }
 
+    /**
+     * Returns the roles granted to the user.
+     *
+     * <code>
+     * public function getRoles()
+     * {
+     *     return array('ROLE_USER');
+     * }
+     * </code>
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        // TODO: Implement getRoles() method.
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        // TODO: Implement getUsername() method.
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
 }
 
